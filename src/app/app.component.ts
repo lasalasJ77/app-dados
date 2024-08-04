440be05ed0987aa6ac7abed89029de7586242af7
JoelL77 <joellasalas77@gmail.com>
2024-08-04 13:31:55 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app-dados';
+
+  dadoIzquierda = 'img/dice1.png';
+  dadoDerecha = 'img/dice4.png';
+  numero1 = 1;
+  numero2 = 2;
+
+  tirarDados(): void {
+    this.numero1 = Math.round(Math.random() * 5) + 1;
+    this.numero2 = Math.round(Math.random() * 5) + 1;
+    this.dadoIzquierda = 'img/dice' + this.numero1 + '.png';
+    this.dadoDerecha = 'img/dice' + this.numero2 + '.png';
+  }
+
 }
